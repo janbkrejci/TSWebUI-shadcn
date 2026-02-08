cb2e02148758d40676465ef1662c6d5623a2fa26
janbkrejci <janbkrejci@gmail.com>
2026-02-08 11:20:35 +0100

    fix: consistent window border color in dark mode

--- src/components/ts-web-ui/ts-window/index.tsx
+++ src/components/ts-web-ui/ts-window/index.tsx
@@ -438,8 +438,8 @@ export const TsWindow = React.forwardRef<TsWindowRef, TsWindowProps>(
         dragHandleClassName="window-drag-handle"
         style={{ zIndex: currentZIndex, opacity: isVisible ? 1 : 0 }}
         className={cn(
-          "flex flex-col overflow-hidden bg-background pointer-events-auto border rounded-lg",
-          windowState === "maximized" ? "" : "shadow-xl dark:border-neutral-700"
+          "flex flex-col overflow-hidden bg-background pointer-events-auto border rounded-lg dark:border-neutral-700",
+          windowState === "maximized" ? "" : "shadow-xl"
         )}
       >
         {/* Header / Titlebar */}
